1097a7da3e7434f4c7bf130cd1116be13ebc0c1a
Pol Rivero <pol.rivero@logicommerce.com>
2024-05-04 20:41:25 +0200

    Squashed commit of the following:
    commit 34d60a5728367afceeea2812272aaec0f5603904
    Merge: 1dfbc3f c583757
    Author: Pol Rivero <pol.rivero@logicommerce.com>
    Date:   Sat May 4 20:37:30 2024 +0200
    
        Merge remote-tracking branch 'origin/pablo'
    
    commit c5837574fbe206353097ed2fe46002826616677f
    Author: PabloJGC <pablo@AlphaTru.localdomain>
    Date:   Sat May 4 20:36:32 2024 +0200
    
        Finished first iteration of vector search algorithm.
    
    commit 1dfbc3f264baf82f8023b95be46b911a61160512
    Author: Pol Rivero <pol.rivero@logicommerce.com>
    Date:   Sat May 4 20:09:49 2024 +0200
    
        Clean up
    
    commit 5415cd3225d931529fc0d77199c7575f873dc056
    Author: Pol Rivero <pol.rivero@logicommerce.com>
    Date:   Sat May 4 17:42:16 2024 +0200
    
        Fix FOUC
    
    commit d1b1109e3f51e8c0afe4f026e49e067fb9187619
    Merge: dba9000 28cad90
    Author: Pol Rivero <pol.rivero@logicommerce.com>
    Date:   Sat May 4 17:05:43 2024 +0200
    
        Merge branch 'main' into pol
    
    commit dba9000301d2c3154c4d5507e6683fd48fb439c0
    Author: Pol Rivero <pol.rivero@logicommerce.com>
    Date:   Sat May 4 17:03:52 2024 +0200
    
        Call backend
    
    commit 28cad90867e34235e254bbd72a8dd4d4e5614823
    Author: Pol Rivero <pol.rivero@logicommerce.com>
    Date:   Sat May 4 15:15:48 2024 +0200
    
        Update

---
+++ ParallelFashion/src/components/getSimilarProductButton.vue
@@ -0,0 +1,46 @@
+<script setup>
+
+import { defineProps } from 'vue'
+
+const props = defineProps(['currentSelectedProductId'])
+
+const ENDPOINT = 'http://localhost:5000/similarProducts'
+
+async function getSimilarProducts() {
+	const id = props.currentSelectedProductId
+	const url = `${ENDPOINT}/${id}`
+	console.log(url)
+  const response = await fetch(url, {
+	method: 'GET',
+	headers: {
+	  'Content-Type': 'application/json'
+	}
+  })
+  const data = await response.json()
+	console.log(data)
+  const ids = data.products
+  const query = `?ids=${ids.join(',')}`
+  window.location.href = `/related${query}`
+}
+
+</script>
+
+<template>
+  <button class="modalButton highlight" @click="getSimilarProducts">
+	Get similar products
+</button>
+</template>
+
+<style scoped>
+.modalButton {
+  background-color: white;
+  color: black;
+  border: none;
+  padding: 10px 20px;
+  margin: 10px;
+  cursor: pointer;
+  &.highlight {
+    background-color: rgb(233, 217, 76);
+  }
+}
+</style>
